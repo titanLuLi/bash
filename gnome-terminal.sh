5aa81eb3b84a15fe3b24ce7b639aef76127570ca
Tian Lu Li <tnl@kmd.dk>
2019-07-30 11:43:52 +0200

    Create gnome-terminal.sh

---
+++ gnome-terminal.sh
@@ -0,0 +1,6 @@
+
+gnome-terminal 
+--tab  -t sktRTTdev11wlsA01 -e "sshpass -p '**' ssh -o StrictHostKeyChecking=no ipowls@host" 
+--tab  -t sktSPKdev10wlsa01 -e "sshpass -p '**' ssh -o StrictHostKeyChecking=no ipowls@Host01" 
+
+
